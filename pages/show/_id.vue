eeb658aba2ed557251e47b9b723a000b936e38f4
Elsa Noblet <elsa.noblet@orange.fr>
2017-05-23 16:36:42 +0200

    tvshow details added on the id page and paddings on nav-bar

--- pages/show/_id.vue
+++ pages/show/_id.vue
@@ -3,15 +3,41 @@
   <div class="container">
     <Affix/>
   <div class="content">
+   <div class="gradient">
+    <div class="backdrop">
+     <img  @click="seen = !seen" style="width:100%; position: relative;
+    top: -50px; z-index: -10;" :src="'https://image.tmdb.org/t/p/original/'+movie.backdrop_path" alt="">
+    <div class="tvshow-title">
+    <p style="font-size: 50px; margin-left: 25px;"><strong>{{movie.original_name}}</strong></p>
+      <p style="font-size: 20px; margin-left: 25px;"><strong>{{movie.first_air_date}}</strong></p>
+      <p style="font-size: 14px; margin-top: -24px; margin-left: 70px;"><strong><li class="tvshow-genres" v-for="genre in movie.genres"> {{genre.name}}</li></strong></p>
+      <p style="font-size: 14px; margin-left: 25px; margin-top: 45px;">{{movie.number_of_seasons}} seasons</p>
+      <p style="font-size: 14px; margin-left: 25px; color: #ff9941;">Last episode aired {{movie.last_air_date}}</p>
+                <div><a href="route"><img src="~assets/FollowButton.png" style="width:130px; height: auto; margin-top: -32px; position: absolute; right: 30px;"/></a></div>
+    </div>
+    </div>
+    </div>
+    <div class="tvshow-synopsis-cast">
+       <div class="tvshow-synopsis">
+       <div id="about"><img src="~assets/about.png" style="width: 8px; margin-right: 5px;"/> About the show</div>
+       <p style="font-size: 20px;">254 Followers</p>
+       <p style="font-size: 20px; margin-bottom: 20px;">26 Critics</p>
+        <p class="text-light" v-if="seen">{{movie.overview}}</p>
+        </div>
+        <div class="tvshow-cast">
+                 <div id="cast">The cast <img src="~assets/cast.png" style="width: 10px; margin-left: 5px;"/></div>
+         <div id="actor-photo-container">
+          <li v-for="actor in cast" id="actor-photo"><img style="width: 130px; height: auto;" :src="'https://image.tmdb.org/t/p/original/'+actor.profile_path"/></li>
+                    <div id="actor-name-container">
+          <li v-for="actor in cast" id="actor-name" style="width: 130px; text-align: center;">{{actor.name}}</li></div></div>
 
-    <img  @click="seen = !seen" style="height:600px;width:auto;" :src="'https://image.tmdb.org/t/p/w500/'+movie.poster_path" alt="">
-    <p><strong>{{movie.original_name}}</strong></p>
+        </div>
+        </div>
     <ul>
       <li v-for="i in movie.number_of_seasons">
         season {{i}}
       </li>
     </ul>
-    <p v-if="seen">{{movie.overview}}</p>
   </div>
 </div>
 </template>
@@ -24,6 +50,7 @@ export default {
   data: () => ({
     movie: {},
     errors: [],
+    cast: [],
     seen: true
   }),
   components: {
@@ -38,7 +65,17 @@ export default {
       // JSON responses are automatically parsed.
 
       this.movie = response.data
-      console.log(this.movie)
+      this.movie.first_air_date = response.data.first_air_date.substring(0, 4)
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+    axios.get('https://api.themoviedb.org/3/tv/' + this.$route.params.id + '/credits?api_key=028097eda8e5dd43094c8fcbaf15a506&language=en-US')
+    .then(response => {
+      // JSON responses are automatically parsed.
+
+      this.cast = response.data.cast
+      console.log(this.cast)
     })
     .catch(e => {
       this.errors.push(e)
@@ -78,16 +115,118 @@ export default {
 }
 </script>
 <style media="screen">
-#container {
+.container {
   display: flex;
   min-height: 100vh;
 }
 .content {
-  display:flex;
+  padding-top: 60px;
   flex: 1 1 auto;
+  z-index: 0;
   background-color: #262835;
   height: 2000px;
   margin-left: 220px;
   justify-content: center;
+  position: relative;
+}
+  
+.backdrop {
+  width: 100%;
+  height: 200px;
+  overflow: hidden;
+  position: relative;
+}
+
+.gradient {
+background-image: -webkit-linear-gradient(left, rgba(0, 0, 0, 0.8) 0%, rgba(255, 255, 255, 0) 100%);
+ position: relative;
+}
+
+.tvshow-title {
+  z-index: 100;
+  position: absolute;
+  top: 10px;
+  width: 100%;
+}
+
+.tvshow-genres {
+  display: inline-block;
+  padding-left: 20px;
+}
+
+.tvshow-synopsis-cast {
+  margin: 20px;
+  padding: 30px 35px;
+  background-color: #161C28;
+}
+
+.tvshow-synopsis {
+  width: 40%;
+  padding-right: 10%;
+  display: inline-block;
+}
+  
+#about {
+  color: white;
+  font-size: 12px;
+  font-weight: 800;
+  margin-top: -5px;
+  margin-left: -15px;
+  margin-bottom: 20px;
+}
+  
+#cast {
+  color: white;
+  font-size: 12px;
+  font-weight: 800;
+  margin-top: -5px;
+  margin-right: -15px;
+  margin-bottom: 20px;
+  text-align: right;
+}  
+  
+.tvshow-cast {
+  display: inline-block;
+  width: 60%;
+  padding-left: 20px;
+  vertical-align: top;
+}
+  
+#actor-name {
+  display: inline-block;
+  padding-left: 0px;
+  color: white;
+  margin-bottom: 10px;
+  font-size: 11px;
+}
+
+#actor-photo {
+  display: inline-block;
+  padding-left: 0px;
+}
+  
+#actor-photo-container {
+  overflow-x: scroll;
+  white-space: nowrap;
+  max-width: 800px;
+  margin-right: 0px;
+}
+  
+::-webkit-scrollbar {
+    width: 12px;
+}  
+  
+::-webkit-scrollbar-track {
+    background: #161C28;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #e8e8e8;
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+}
+  
+.text-light {
+  font-weight: 400;
 }
 </style>
