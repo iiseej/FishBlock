ef10cd0b53d8c6c74c68387af7fea2f49d8f8546
iiseej <fmalmasson@hotmail.com>
2017-05-24 09:12:45 +0200

    add apiKey as a variable

--- pages/home.vue
+++ pages/home.vue
@@ -4,6 +4,14 @@
     <div class="home-content">
       <Searchbar/>
       <h1>{{msg}}</h1>
+      <div id="wrapper">
+    <div style="position: absolute; left: 10px; top: 10px; width:200px; height:100px; background-color: yellow;">
+        abcd
+    </div>
+    <div style="position: absolute; z-index: 1; left: 50px; top: 20px; width:100px; height:20px; background-color: green;">
+        1234
+    </div>
+</div>
     </div>
   </div>
 
@@ -11,7 +19,7 @@
 
 <script>
 import Affix from '~components/Affix.vue'
-import Searchbar from '~components/Searchbar.vue'
+
 export default {
   data () {
     return {
@@ -19,8 +27,7 @@ export default {
     }
   },
   components: {
-    Affix,
-    Searchbar
+    Affix
   }
 
 }
